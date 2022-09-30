482041eea9fc6385ef45481bac0ec45d484b9ad9
ACER <ACER@DESKTOP-VO7D3LL>
2022-09-30 21:48:49 +0530

    first

---
+++ src/main/java/VFS_Global_Automation/VFS_Global_Automation/App.java
@@ -0,0 +1,13 @@
+package VFS_Global_Automation.VFS_Global_Automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
